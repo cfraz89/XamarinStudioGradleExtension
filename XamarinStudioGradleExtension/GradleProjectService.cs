da6f7a6ce27cd54fda055f45be819b2ccb870c35
FRASER, Chris <Chris.FRASER@suncorp.com.au>
2014-07-01 13:45:33 +1000

    Fixed extension to not apply against mdtool

--- XamarinStudioGradleExtension/GradleProjectService.cs
+++ XamarinStudioGradleExtension/GradleProjectService.cs
@@ -12,14 +12,20 @@ namespace XamarinStudioGradleExtension
 
 		protected override BuildResult Build (MonoDevelop.Core.IProgressMonitor monitor, SolutionEntityItem item, ConfigurationSelector configuration)
 		{
-			ItemConfiguration itemConfig = configuration.GetConfiguration (item);
-			var projectGradleInterface = new ProjectGradleInterface (item as Project);
-
-			var result = new BuildResult();
-			if (projectGradleInterface.DetectedGradleFile && itemConfig != null)
-				result.Append(new GradleBuilder (monitor, item, configuration).Build ());
-
-			result.Append(base.Build (monitor, item, configuration));
+			var result = new BuildResult ();
+			//mdtool is derp, and it will still apply this plugin. That will cause a recursive compile. We don't want that.
+			if (monitor.GetType() != typeof(MonoDevelop.Core.ProgressMonitoring.ConsoleProgressMonitor)) {
+				ItemConfiguration itemConfig = configuration.GetConfiguration (item);
+				var projectGradleInterface = new ProjectGradleInterface (item as Project);
+				try {
+					if (projectGradleInterface.PropertiesForProject ().UseGradle && itemConfig != null)
+						result.Append (new GradleBuilder (monitor, item, configuration).Build ());
+				} catch (Exception e) {
+				} finally {
+					result.Append (base.Build (monitor, item, configuration));
+				}
+			} else
+				result = base.Build (monitor, item, configuration);
 
 			return result;
 		}
